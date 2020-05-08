74530b8e45500bf49a27f2d08f39faf916e855f3
Adrian <adrianpadilla@outlook.com>
2020-05-08 00:41:28 -0700

    Added missing python file

---
+++ src/putting_it_together/ch9.py
@@ -0,0 +1,61 @@
+from src.Plane import plane
+from src.Sphere import sphere
+from src.Matrix import Matrix
+from src.Material import Material
+from src.Color import Color
+from src.World import World, view_transform
+from src.PointLight import PointLight
+from src.Vector import point, vector
+from src.Camera import Camera
+
+
+def ch9():
+    # Step 1
+    floor = plane()
+    floor.transform = Matrix.scaling(10, 0.01, 10)
+    floor.material = Material()
+    floor.material.color = Color(1, 0.9, 0.9)
+    floor.material.specular = 0
+
+    # Middle biggest sphere
+    middle = sphere()
+    middle.transform = Matrix.translation(-0.5, 1, 0.5)
+    middle.material = Material()
+    middle.material.color = Color(0.1, 1, 0.5)
+    middle.material.diffuse = 0.7
+    middle.material.specular = 0.3
+
+    # Smaller right sphere
+    right = sphere()
+    right.transform = Matrix.translation(1.5, 0.5, -0.5) * Matrix.scaling(0.5, 0.5, 0.5)
+    right.material = Material()
+    right.material.color = Color(0.5, 1, 0.1)
+    right.material.diffuse = 0.7
+    right.material.specular = 0.3
+
+    # Left yellow sphere
+    left = sphere()
+    left.transform = Matrix.translation(-1.5, 0.33, -0.75) * Matrix.scaling(0.33, 0.33, 0.33)
+    left.material = Material()
+    left.material.color = Color(1, 0.8, 0.1)
+    left.material.diffuse = 0.7
+    left.material.specular = 0.3
+
+    world = World()
+    world.light = PointLight(point(-10, 10, -10), Color(1, 1, 1))
+    world.objects.extend([floor, middle, right, left])
+
+    # camera = Camera(100, 50, 60)
+    camera = Camera(500, 250, 60)
+    camera.transform = view_transform(point(0, 1.5, -5),
+                                      point(0, 1, 0),
+                                      vector(0, 1, 0))
+
+    canvas = camera.render(world)
+
+    with open('ch9.ppm', 'w') as fp:
+        fp.write(canvas.to_ppm())
+
+
+if __name__ == '__main__':
+    ch9()
